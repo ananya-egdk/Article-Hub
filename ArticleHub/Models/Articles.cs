e048e9b812da71b2b7267e0cb99b7fc774375e71
anaae <anaae@AUTOE7PEH9WWMV7>
2025-01-15 10:18:48 +0530

    Add project files.

---
+++ ArticleHub/Models/Articles.cs
@@ -0,0 +1,10 @@
+﻿namespace ArticleHub.Models
+{
+    public class Articles
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Summary { get; set; }
+        public string FullText { get; set; }
+    }
+}
---
+++ ArticleHub/Services/ArticleService.cs
@@ -0,0 +1,39 @@
+﻿using ArticleHub.Models;
+using System.Net.Http.Json;
+
+namespace ArticleHub.Services
+{
+    public class ArticleService
+    {
+        private readonly HttpClient _httpClient;
+
+        public ArticleService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<List<Articles>> GetArticlesAsync()
+        {
+            try
+            {
+                return await _httpClient.GetFromJsonAsync<List<Articles>>("https://ps-dev-1-partnergateway.patientsky.dev/assignment/articles");
+            }
+            catch (Exception)
+            {
+                throw new Exception("Failed to fetch articles.");
+            }
+        }
+
+        public async Task<Articles> GetArticleByIdAsync(int id)
+        {
+            try
+            {
+                return await _httpClient.GetFromJsonAsync<Articles>($"https://ps-dev-1-partnergateway.patientsky.dev/assignment/articles/{id}");
+            }
+            catch (Exception)
+            {
+                throw new Exception("Failed to fetch the article details.");
+            }
+        }
+    }
+}
